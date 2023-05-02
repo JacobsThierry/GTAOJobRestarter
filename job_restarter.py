86414718ad2018d07a78aa614957d08d66e7c4ae
Thierry Jacobs <thierry.jm35@gmail.com>
2023-05-02 18:16:41 +0200

    renamed pattern

--- job_restarter.py
+++ job_restarter.py
@@ -7,7 +7,7 @@ import numpy as np
 import os
 from datetime import datetime
 
-pattern = cv2.imread(os.path.join(os.getcwd(), "./pattern2.png"))
+pattern = cv2.imread(os.path.join(os.getcwd(), "./pattern.png"))
 
 
 def waitOnPress(key):
